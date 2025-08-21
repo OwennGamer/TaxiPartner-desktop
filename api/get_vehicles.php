5ce653648124ad0e406fdc6873508c5f4bb06360
PC <pawel.turek330@gmail.com>
2025-08-21 09:42:57 +0200

    21.08.2025

--- api/get_vehicles.php
+++ api/get_vehicles.php
@@ -5,6 +5,14 @@ require_once "db.php";
 try {
     $stmt = $pdo->query("SELECT id, rejestracja, marka, model, przebieg, ubezpieczenie_do, przeglad_do, aktywny, inpost, taxi, taksometr, legalizacja_taksometru_do, gaz, homologacja_lpg_do, firma, forma_wlasnosci, numer_polisy FROM pojazdy");
     $vehicles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($vehicles as &$v) {
+        $v['aktywny'] = (int)$v['aktywny'] === 1;
+        $v['inpost'] = (int)$v['inpost'] === 1;
+        $v['taxi'] = (int)$v['taxi'] === 1;
+        $v['taksometr'] = (int)$v['taksometr'] === 1;
+        $v['gaz'] = (int)$v['gaz'] === 1;
+    }
+    unset($v);
     echo json_encode(["status" => "success", "data" => $vehicles]);
 } catch (PDOException $e) {
     echo json_encode(["status" => "error", "message" => "Błąd bazy danych"]);
